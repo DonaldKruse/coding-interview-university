bb49db8a6ad65192cad59bba4fc180024d280120
Donald Kruse <dkmelodee@gmail.com>
2019-04-23 15:53:08 -0600

    implimented recursive version

--- myimpl/more_knowledge/binary_search/binarysearch.c
+++ myimpl/more_knowledge/binary_search/binarysearch.c
@@ -18,7 +18,6 @@ int binary_search_iter(int needle, int* array, const int size)
      for (i = 0; i < MAX; i++)
      {
 	  val = array[index];
-	  printf("array[%d] = %d\n", index, val);
 	  if (val < needle)
 	  {
 	       low = index + 1;
@@ -39,10 +38,32 @@ int binary_search_iter(int needle, int* array, const int size)
      return -1;
 }
 
+int binary_search_recur(int needle, int* array, int low, int high)
+{
+     if (high >= low)
+     {
+	  int index = (low + high) / 2;
+
+	  if ( array[index] < needle )
+	       low = index + 1;
+	  if ( array[index] > needle )
+	       high = index - 1;
+	  if ( array[index] == needle)
+	       return index;
+	  return binary_search_recur(needle, array, low, high);
+     }	  
+     return -1;
+}
+     
 int main()
 {
      int arr[10] = {1,3,5,6,7,8,11,12,13,55};
      int foundat = binary_search_iter(55, arr, 10);
-     printf("foundat %d\n", foundat);
+     int i;
+     for (i = 0; i < 10; i++)
+	  printf("foundat %d\n", binary_search_iter(arr[i], arr, 10));
+     for (i = 0; i < 10; i++)
+	  printf("foundat %d\n", binary_search_recur(arr[i], arr, 0, 10));
+
      return 0;
 }
