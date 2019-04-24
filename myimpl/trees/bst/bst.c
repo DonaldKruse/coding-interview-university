17c6b858f17004e73b7f836706ea40113452bfe0
Donald Kruse <dkmelodee@gmail.com>
2019-04-24 12:45:16 -0600

    add min, max and insert functions for bst

--- myimpl/trees/bst/bst.c
+++ myimpl/trees/bst/bst.c
@@ -3,7 +3,7 @@
 
 struct Node
 {
-     int val;
+     int data;
      struct Node* left;
      struct Node* right;
 };
@@ -13,21 +13,65 @@ typedef struct Node Node;
 Node* create_node(int val)
 {
      Node* node = malloc(sizeof(Node));
-     node->val = val;
+     node->data = val;
      node->left = NULL;
      node->right = NULL;
      return node;
 }
 
-void insert(Node** root_ref, int data)
+Node* insert(Node* root, int data)
 {
-     Node* root = *(root_ref);
      if (!root)
+	  return create_node(data);
+     if (data <= root->data)
+	  root->left = insert(root->left, data);
+     else
+	  root->right = insert(root->right, data);
+     return root;
+}
+
+int bst_min(Node* root)
+{
+     if(!root)
      {
-	  root = create_node(data);
+	  printf("null root: no minimum\n");
+	  return -1;
      }
      
+     Node* current = root;
+     while (current->left)
+	  current = current->left;
+
+     return current->data;
+}
+
+
+int bst_max(Node* root)
+{
+     if(!root)
+     {
+	  printf("null root: no minimum\n");
+	  return -1;
+     }
+     
+     Node* current = root;
+     while (current->right)
+	  current = current->right;
+
+     return current->data;
+}
+
+
 int main ()
 {
      Node* root = NULL;
-     
+     root = insert(root, 10);
+     root = insert(root, 5);
+     root = insert(root, 8);
+     root = insert(root, 2);
+     root = insert(root, 20);
+
+     printf("min = %d\n", bst_min(root));
+     printf("max = %d\n", bst_max(root));
+     return 0;
+}
